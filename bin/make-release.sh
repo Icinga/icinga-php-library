12ab208a186a33092aed14a588779939b64f5ebb
Eric Lippmann <eric.lippmann@icinga.com>
2026-03-26 14:07:33 +0100

    Use `git tag -s`

--- bin/make-release.sh
+++ bin/make-release.sh
@@ -54,7 +54,7 @@ git checkout vendor
 composer validate --no-check-all --strict || fail "Composer validate failed"
 
 if [ -z "$NO_OPT" ]; then
-  git tag -a v$VERSION -m "Version v$VERSION"
+  git tag -s v$VERSION -m "Version v$VERSION"
   echo "Finished, tagged v$VERSION"
   echo "Now please run:"
 else
